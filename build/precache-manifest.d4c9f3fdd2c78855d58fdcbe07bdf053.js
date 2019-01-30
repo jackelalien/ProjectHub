27c7ec0efd012c3e2ef4dc0d7ed8867dc8a2272b
jackelalien <jxl7581@louisiana.edu>
2019-01-30 15:54:11 -0600

    Add files via upload

---
+++ build/precache-manifest.d4c9f3fdd2c78855d58fdcbe07bdf053.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "02ad94630eb42873bfa7",
+    "url": "/static/css/3.e661f318.chunk.css"
+  },
+  {
+    "revision": "27ec383236579ed3f04a",
+    "url": "/static/css/main.4f9efd80.chunk.css"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/static/media/burger-logo.b8503d26.png"
+  },
+  {
+    "revision": "ef893ff89a7a6f8a0b92",
+    "url": "/static/js/1.ef893ff8.chunk.js"
+  },
+  {
+    "revision": "8905000c12bbea1e15bb",
+    "url": "/static/js/runtime~main.8905000c.js"
+  },
+  {
+    "revision": "61d2258f2760eebf28b3",
+    "url": "/static/js/2.61d2258f.chunk.js"
+  },
+  {
+    "revision": "27ec383236579ed3f04a",
+    "url": "/static/js/main.27ec3832.chunk.js"
+  },
+  {
+    "revision": "02ad94630eb42873bfa7",
+    "url": "/static/js/3.02ad9463.chunk.js"
+  },
+  {
+    "revision": "b287df839579f5effb90",
+    "url": "/static/js/4.b287df83.chunk.js"
+  },
+  {
+    "revision": "61d2258f2760eebf28b3",
+    "url": "/static/css/2.445e6306.chunk.css"
+  },
+  {
+    "revision": "ef893ff89a7a6f8a0b92",
+    "url": "/static/css/1.7f987ed6.chunk.css"
+  },
+  {
+    "revision": "92a383018ff8e0fc46a6ab095ad42d18",
+    "url": "/index.html"
+  }
+];
